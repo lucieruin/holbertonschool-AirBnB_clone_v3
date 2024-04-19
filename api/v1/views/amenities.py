4d961e7c6c528dc4069333c531a4d9d656b225e4
lucieruin <lucie.wattez@hotmail.fr>
2024-04-19 17:25:43 +0200

    correct pycodestyle for file init,amenities, places_reviews, users & add places api

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -23,7 +23,8 @@ def get_amenities():
 
 # GET a amenity by id
 # ============================================================================
-@app_views.route('/amenities/<amenity_id>', methods=['GET'], strict_slashes=False)
+@app_views.route('/amenities/<amenity_id>', methods=['GET'],
+                 strict_slashes=False)
 def get_one_amenity(amenity_id):
     """ retrieves a amenitie by id """
     amenity = storage.all(Amenity, amenity_id)
@@ -35,7 +36,8 @@ def get_one_amenity(amenity_id):
 
 # DELETE one amenity (id)
 # ============================================================================
-@app_views.route('/amenities/<amenity_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/amenities/<amenity_id>', methods=['DELETE'],
+                 strict_slashes=False)
 def delete_amenity(amenity_id):
     """ delete a amenitie by id """
     amenity = storage.all(Amenity, amenity_id)
@@ -56,10 +58,10 @@ def create_amenity():
 
     if data is None:
         abort(400, description="Not a JSON")
-    
+
     if "name" not in data:
         abort(400, description="Missing name")
-    
+
     new_amenity = Amenity(** request.json)
     new_amenity.save()
     return jsonify(new_amenity.to_dict()), 201
@@ -67,14 +69,15 @@ def create_amenity():
 
 # PUT: update an amenity
 # ============================================================================
-@app_views.route('/amenities/<amenity_id>', methods=['PUT'], strict_slashes=False)
+@app_views.route('/amenities/<amenity_id>', methods=['PUT'],
+                 strict_slashes=False)
 def update_amenity(amenity_id):
     """ PUT: update an amenity """
-    amenity =storage.get(Amenity, amenity_id)
+    amenity = storage.get(Amenity, amenity_id)
 
     if amenity is None:
         abort(404)
-    
+
     data = request.get_json()
 
     if data is None:
--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -10,12 +10,15 @@ from models.user import User
 
 # GET all reviews
 # ============================================================================
-@app_views.route("/places/<place_id>/reviews", methods=["GET"], strict_slashes=False)
+@app_views.route("/places/<place_id>/reviews", methods=["GET"],
+                 strict_slashes=False)
 def get_reviews(place_id):
     """Retrieves the list of all Review objects of a Place"""
     place = storage.get(Place, place_id)
+
     if place is None:
         abort(404)
+
     reviews = storage.all(Review).values()
     reviews_list = [
         review.to_dict() for review in reviews if review.place_id == place_id
@@ -25,23 +28,29 @@ def get_reviews(place_id):
 
 # GET one review (id)
 # ============================================================================
-@app_views.route("/reviews/<review_id>", methods=["GET"], strict_slashes=False)
+@app_views.route("/reviews/<review_id>", methods=["GET"],
+                 strict_slashes=False)
 def get_review(review_id):
     """Retrieves a Review object"""
     review = storage.get(Review, review_id)
+
     if review is None:
         abort(404)
+
     return jsonify(review.to_dict())
 
 
 # DELETE one review (id)
 # ============================================================================
-@app_views.route("/reviews/<review_id>", methods=["DELETE"], strict_slashes=False)
+@app_views.route("/reviews/<review_id>", methods=["DELETE"],
+                 strict_slashes=False)
 def delete_review(review_id):
     """Deletes a Review object"""
     review = storage.get(Review, review_id)
+
     if review is None:
         abort(404)
+
     storage.delete(review)
     storage.save()
     return jsonify({}), 200
@@ -49,24 +58,31 @@ def delete_review(review_id):
 
 # POST (create a review)
 # ============================================================================
-@app_views.route("/places/<place_id>/reviews", methods=["POST"], strict_slashes=False)
-def create_review(place_id):
-    """Creates a Review"""
+@app_views.route("/places/<place_id>/reviews", methods=["POST"],
+                 strict_slashes=False)
+def post_review(place_id):
+    """ Creates a review """
     place = storage.get(Place, place_id)
     if place is None:
         abort(404)
-    req_json = request.get_json()
-    if req_json is None:
+
+    data = request.get_json()
+
+    if data is None:
         abort(400, description="Not a JSON")
-    if "user_id" not in request.get_json():
+
+    if "user_id" not in data:
         abort(400, description="Missing user_id")
-    user = storage.get(User, request.get_json["user_id"])
+
+    user = storage.get(User, data["user_id"])
     if user is None:
         abort(404)
-    if "text" not in req_json():
+
+    if "text" not in data:
         abort(400, description="Missing text")
-    req_json["place_id"] = place_id
-    review = Review(**req_json)
+
+    data["place_id"] = place_id
+    review = Review(**data)
     review.save()
     return jsonify(review.to_dict()), 201
 
@@ -74,17 +90,26 @@ def create_review(place_id):
 # PUT (update a review)
 # ============================================================================
 @app_views.route("/reviews/<review_id>", methods=["PUT"], strict_slashes=False)
-def update_review(review_id):
-    """Updates a Review object"""
+def put_review(review_id):
+    """ Updates a review by its id """
     review = storage.get(Review, review_id)
+
     if review is None:
         abort(404)
-    req_json = request.get_json()
-    if req_json is None:
+
+    data = request.get_json()
+
+    if data is None:
         abort(400, description="Not a JSON")
-    ignore_keys = ["id", "user_id", "place_id", "created_at", "updated_at"]
-    for key, value in req_json.items():
-        if key not in ignore_keys:
+
+    for key, value in data.items():
+        if key not in ["id",
+                       "user_id",
+                       "place_id",
+                       "created_at",
+                       "updated_at"
+                       ]:
             setattr(review, key, value)
+
     review.save()
     return jsonify(review.to_dict()), 200
