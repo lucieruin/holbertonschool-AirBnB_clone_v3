a46dca29ddb8cf15c7a205a8be59b62f9adc1f4c
lucieruin <lucie.wattez@hotmail.fr>
2024-04-19 18:14:48 +0200

    correct answer for request if state is not data

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -91,7 +91,7 @@ def put_state(state_id):
         abort(400, description="Not a JSON")
 
     if "id" in data and data["id"] != state_id:
-        abort(400, description="not match id in JSON data")
+        abort(404)
 
     for key, value in data.items():
         if key not in ["id", "created_at", "updated_at"]:
